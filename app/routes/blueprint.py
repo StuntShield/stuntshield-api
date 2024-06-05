46ea9496a417d3f8c0adec1f0192280433ccd5ef
sayid <sayid.10121012@mahasiswa.unikom.ac.id>
2024-06-05 14:37:55 +0700

    added routes

--- app/routes/blueprint.py
+++ app/routes/blueprint.py
@@ -1,12 +1,13 @@
 from flask import Blueprint
 
-from .controllers.StuntingController import StuntingController
+from .controllers.LLMController import getPrompt
 
 blueprint = Blueprint('blueprint', __name__)
 
 
 # define the routes
-blueprint.route('/', methods=['GET'])(StuntingController.index)
+# blueprint.route('/', methods=['GET'])(StuntingController.index)
+blueprint.route('/prompt', methods=['GET'])(getPrompt)
 # blueprint.route("/stunting-classification", methods=["GET"])(index)
 # blueprint.route("/create", methods=["GET"])(create)
 # blueprint.route("/insert", methods=["GET"])(insert)
