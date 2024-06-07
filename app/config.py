e61316cad32c5e1f5028a5c54a544a61cc25c312
sans18-coder <iksanwijaya018@gmail.com>
2024-06-07 18:33:16 +0700

    API Predict,untuk model 2 lagi menyusul

--- app/config.py
+++ app/config.py
@@ -1,11 +1,17 @@
 main_config = {
     'BASE_URL': 'http://localhost',
     'PORT': '5000',
+    'UPLOAD_FOLDER_STUNTING':'./assets/users_uploads/data_stunting',
+    'UPLOAD_FOLDER_IDEAL_024':'./assets/users_uploads/data_stunting/umur_0_24_bulan',
+    'UPLOAD_FOLDER_IDEAL_2460':'./assets/users_uploads/data_stunting/umur_24_60_bulan',
+    # 'GCS_CREDENTIALS': '',
     # 'GCP_CREDIT': ''
 }
 
 model_config = {
-    # 'UPLOAD_FOLDER':'',
-    # 'MODEL_CLASSIFICATION' : '',
+    'MODEL_CLASSIFICATION_STUNTING' : './assets/ml_models/stunting_prediction.h5',
+    'MODEL_CLASSIFICATION_IDEAL_024' : './assets/ml_models/ideal_prediction_024.h5',
+    'MODEL_CLASSIFICATION_IDEAL_2460' : './assets/ml_models/ideal_prediction_2460.h5',
+    
     # 'OPENAI_API_KEY': ''
 }
--- app/routes/controllers/StuntingController.py
+++ app/routes/controllers/StuntingController.py
@@ -1,20 +1,127 @@
-from flask import jsonify
-from flask import request
+import os; os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import pandas as pd
+import numpy as np
+import csv
+import random
+from http import HTTPStatus
+import tensorflow as tf
+from flask import Flask, jsonify, request
+from google.cloud import storage
+from dotenv import load_dotenv
+from ...config import model_config as mconfig
+from ...config import main_config as config
 
+load_dotenv()
 
 class StuntingController:
-
-    # get all data
+    bucket_name = os.environ.get('BUCKET_NAME','data-balita')
+    client = storage.Client.from_service_account_json(json_credentials_path=config['GCS_CREDENTIALS'])
+    bucket = storage.Bucket(client,bucket_name)
+    
+    model_stunting = tf.keras.models.load_model(mconfig['MODEL_CLASSIFICATION_STUNTING'],compile=False)
+    model_ideal_024 = tf.keras.models.load_model(mconfig['MODEL_CLASSIFICATION_IDEAL_024'],compile=False)
+    model_ideal_2460 = tf.keras.models.load_model(mconfig['MODEL_CLASSIFICATION_IDEAL_2460'],compile=False)
+    classes_stunting = ["Stunting Berat","Stunting","Normal","Tinggi"] 
+    classes_ideal = ["Severely Wasted","Wasted","Normal","Berisiko Overweight","Overweight","Obesitas"] 
     def index():
-
-        # get request parameter
-        username = request.args.get('username')
-        password = request.args.get('password')
-
-        return jsonify(
-            {'data_test': {'username': username, 'password': password}}
-        )
-
-    # show specific user
-    def show():
-        pass
+        return jsonify({
+        "status" : {
+            "code" : HTTPStatus.OK,
+            "message" : "nyambung cuy santui",
+        },
+        "data" : None
+    }),HTTPStatus.OK
+        
+    def upload_data(fieldnames,data,csv_file,path):
+        file_csv = csv_file 
+        file_path = os.path.join(path,file_csv)
+        with open(file_path, mode='w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerow(data)
+        blob = StuntingController.bucket.blob('Users_input/'+file_csv+str(random.randint(10000,99999)) )
+        blob.upload_from_filename(file_path)
+        os.remove(file_path)
+        
+    def predict_stunting():
+        if request.method == "POST":
+            year = float(request.form["year"])
+            month = float(request.form["month"])
+            day = float(request.form["day"])
+            jenis_kelamin = request.form["jenis_kelamin"]
+            tinggi_badan = float(request.form["tinggi_badan"])
+            berat_badan = float(request.form["berat_badan"])
+            if year is not None and month is not None and day is not None and jenis_kelamin is not None and tinggi_badan is not None and berat_badan is not None:
+                umur = ((year*12) + month) + (day/30)
+                jenis_kelamin_map = {'laki-laki': 0, 'perempuan': 1}
+                jenis_kelamin_num = jenis_kelamin_map[jenis_kelamin]
+                fieldstunting =["Umur (bulan)", "Jenis Kelamin", "Tinggi Badan (cm)","Status Gizi"]
+                fieldideal=["Jenis Kelamin", "Tinggi Badan (cm)", "Berat Badan (kg)", "Status"]
+                input_data_stunting_predict = pd.DataFrame({'Umur': [umur], 'Jenis Kelamin': [jenis_kelamin_num], 'Tinggi Badan': [tinggi_badan]})
+                prediction_stunting_result = StuntingController.model_stunting.predict(input_data_stunting_predict)
+                
+                data_stunting = {
+                    "Umur (bulan)" : umur,
+                    "Jenis Kelamin" : jenis_kelamin,
+                    "Tinggi Badan (cm)" : tinggi_badan,
+                    "Status Gizi" : StuntingController.classes_stunting[np.argmax(prediction_stunting_result)]
+                }
+                StuntingController.upload_data(fieldstunting,data_stunting,'inputan_stunting.csv',config['UPLOAD_FOLDER_STUNTING'])
+                
+                if umur <= 24 :
+                    input_data_ideal_024_predict = pd.DataFrame({'Jenis Kelamin': [jenis_kelamin_num], 'Tinggi Badan': [tinggi_badan], 'Berat Badan' : [berat_badan]})
+                    prediction_ideal_result = StuntingController.model_ideal_024.predict(input_data_ideal_024_predict)
+                    data_ideal ={
+                        "Jenis Kelamin" : jenis_kelamin,
+                        "Tinggi Badan (cm)" : tinggi_badan,
+                        "Berat Badan (kg)" : berat_badan,
+                        "Status" : StuntingController.classes_ideal[np.argmax(prediction_ideal_result)], 
+                    }
+                    StuntingController.upload_data(fieldideal,data_ideal,'inputan_ideal_024.csv',config['UPLOAD_FOLDER_IDEAL_024'])
+                else :
+                    input_data_ideal_2460_predict = pd.DataFrame({'Jenis Kelamin': [jenis_kelamin_num], 'Tinggi Badan': [tinggi_badan], 'Berat Badan' : [berat_badan]})
+                    prediction_ideal_result = StuntingController.model_ideal_2460.predict(input_data_ideal_2460_predict)
+                    data_ideal ={
+                        "Jenis Kelamin" : jenis_kelamin,
+                        "Tinggi Badan (cm)" : tinggi_badan,
+                        "Berat Badan (kg)" : berat_badan,
+                        "Status" : StuntingController.classes_ideal[np.argmax(prediction_ideal_result)], 
+                    }
+                    StuntingController.upload_data(fieldideal,data_ideal,'inputan_ideal_2460.csv',config['UPLOAD_FOLDER_IDEAL_2460'])
+                
+                result_prediction_stunting = {
+                    'class' : StuntingController.classes_stunting[np.argmax(prediction_stunting_result)],
+                    'presentase' : str("{:.1f}".format(np.max(prediction_stunting_result)*100))
+                }
+                result_prediction_ideal ={
+                    'class' : StuntingController.classes_ideal[np.argmax(prediction_ideal_result)],
+                    'presentase' : str("{:.1f}".format(np.max(prediction_ideal_result)*100))
+                }
+                
+                result_all = {
+                    'stunting' : result_prediction_stunting,
+                    'ideal' : result_prediction_ideal,
+                }
+                return jsonify({
+                    "status" : {
+                        "code" : HTTPStatus.OK,
+                        "message" : "Success predicting",
+                    },
+                    "data" : result_all,
+                }),HTTPStatus.OK
+            else :
+                return jsonify({
+                    "status" : {
+                        "code" : HTTPStatus.BAD_REQUEST,
+                        "message": "Client side error"
+                    },
+                    "data": None
+                }),HTTPStatus.BAD_REQUEST
+        else:
+            return jsonify({
+                "status" : {
+                    "code": HTTPStatus.METHOD_NOT_ALLOWED,
+                    "message" : "Method not allowed",
+                },
+                "data": None,
+            }),HTTPStatus.METHOD_NOT_ALLOWED
