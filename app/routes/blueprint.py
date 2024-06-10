e518e215b7777d257492c42f1dfc3bcd5f6141d1
sayid <sayid.10121012@mahasiswa.unikom.ac.id>
2024-06-10 10:14:37 +0700

    fixed the recommendation message

--- app/routes/blueprint.py
+++ app/routes/blueprint.py
@@ -1,13 +1,14 @@
 from flask import Blueprint
 
 from .controllers.LLMController import getPrompt
-from .controllers.StuntingController import StuntingController
+
+# from .controllers.StuntingController import StuntingController
 blueprint = Blueprint('blueprint', __name__)
 
 
 # define the routes
-blueprint.route('/', methods=['GET'])(StuntingController.index)
+# blueprint.route('/', methods=['GET'])(StuntingController.index)
 blueprint.route('/prompt', methods=['GET'])(getPrompt)
-blueprint.route("/stunting-classification", methods=["POST"])(StuntingController.predict_stunting)
+# blueprint.route("/stunting-classification", methods=["POST"])(StuntingController.predict_stunting)
 # blueprint.route("/create", methods=["GET"])(create)
 # blueprint.route("/insert", methods=["GET"])(insert)
--- app/routes/controllers/LLMController.py
+++ app/routes/controllers/LLMController.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 import json
 from openai import OpenAI, AssistantEventHandler
+from ...config import API_KEY
 
 
 def getPrompt():
@@ -9,8 +10,14 @@ def getPrompt():
     status = 'stunted,overweight'
     berat_badan = 65
     jenis_kelamin = 'laki-laki'
-    client = OpenAI()
+    messages = getPromptLocally(
+        tinggi_badan, usia, status, berat_badan, jenis_kelamin
+    )
+    return jsonify({'success': 'OK', 'message': str(messages)}), 200
 
+
+def getPromptLocally(tinggi_badan, usia, status, berat_badan, jenis_kelamin):
+    client = OpenAI()
     # Create a new thread
     thread = client.beta.threads.create()
     input_message = 'tinggi badan: {}, usia: {} tahun, status_gizi:{}, berat badan:{}, jenis kelamin:{}'.format(
@@ -23,20 +30,17 @@ def getPrompt():
 
     # Define the instructions for the assistant
     instructions = 'Berikan rekomendasi makanan anak - anak berdasarkan input. tampilkan berupa list dan juga berikan rekomendasi penanganannya'
+    assistant_id = 'asst_BZ2loRreVsga2g26VYYrSWAi'
 
-    assistant = client.beta.assistants.create(
-        name='StuntShield Doctor',
-        instructions=instructions,
-        model='gpt-4o',
-    )
     run = client.beta.threads.runs.create_and_poll(
         thread_id=thread.id,
-        assistant_id=assistant.id,
-        instructions='sebutkan usia, tinggi badan, berat badan, jenis kelamin, status gizi terlebih dahulu, lalu Berikan rekomendasi makanan anak - anak berdasarkan input. tampilkan berupa list dan juga berikan rekomendasi penanganannya',
+        assistant_id=assistant_id,
+        instructions=instructions,
     )
 
     messages = 'gagal membuat rekomendasi.'
     if run.status == 'completed':
         messages = client.beta.threads.messages.list(thread_id=thread.id)
         messages = messages.data[0].content[0].text.value
-    return jsonify({'success': 'OK', 'message': str(messages)}), 200
+
+    return str(messages)
